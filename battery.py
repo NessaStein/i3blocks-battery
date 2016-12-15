a72f4205ddb00ee0bc6417f9ff94fafee860051b
xenbyte <akbashev17@gmail.com>
2016-12-15 18:43:01 +0500

    refact

--- battery.py
+++ battery.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
 import subprocess
 
-battery = subprocess.check_output([ "acpi" ], universal_newlines=True)
+battery = subprocess.check_output(["acpi"], universal_newlines=True)
 if not battery:
     output = '<span font="FontAwesome" color="#ffffff">\f12a Error</span>'
 else:
     battery_information = battery.split("\n")[0].split(", ")
     state = battery_information[0].split(":")[1]
     percent = int(battery_information[1].rstrip("%"))
-    time = battery_information[2].split(" ")[0]
-    # if state != "Full": time = battery_information[2].split(" ")[0]
-    # else: time = "FULL"
+    if state != " Full":
+        time = battery_information[2].split(" ")[0]
+    else:
+        time = "FULL"
+
     class Status(object):
         color = ""
         icon = ""
         percent = ""
         time = ""
+
         def __init__(self, color, icon, percent, time):
             self.color = color
             self.icon = icon
             self.percent = str(percent) + "% "
             self.time = time
+
     def status_view(percent):
         if percent <= 3:
             # subprocess.call(['notify-send', time])
@@ -36,10 +40,22 @@ else:
         if percent <= 75:
             return Status("#FFFFFF", "\uf241 ", percent, time)
         return Status("#FFFFFF", "\uf240 ", percent, time)
-    # def state_view(state):
-        # if state == "Disharging": return ""
-        # else: return "\f1e6"
+
+    def state_view(state):
+        if state == " Discharging":
+            return ""
+        else:
+            return "\uf1e6"
+
     status = status_view(percent)
-    template = '<span font="FontAwesome"><span>{}</span><span color="{}">{}</span><span font="Roboto">{}<span>[{}]</span></span></span>'
-    output = template.format("", status.color, status.icon, str(status.percent), status.time)
+    template = '''<span font="FontAwesome"><span>{}</span><span color="{}">{}</span><span font="Roboto">{}<span>[{}]</span></span></span>'''
+    print('\nSTATE', state, '\n')
+
+    output = template.format(
+        state_view(state),
+        status.color,
+        status.icon,
+        str(status.percent),
+        status.time)
+
 print(output)
